8ec67de2a8fec625e3d3e3090adb5e35665de09e
Lupa Stance <genzokun@gmail.com>
2026-01-16 20:30:25 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import { ref } from 'vue';
+  import Pletina from './components/Pletina.vue'
+  import track01 from '@/assets/track-01.mp3'
+
+  const player = ref(null)
+
+  function playContent(){
+    console.log(player.value);
+    player.value.play()
+    
+  }
+</script>
+
+<template>
+  <audio ref="player">
+    <source :src="track01" type="audio/mpeg">
+  </audio>
+  <Pletina icon="iconBw" @clickIcon="playContent" />
+  <Pletina icon="iconPlay" @clickIcon="playContent" />
+  <Pletina icon="iconPause" />
+  <Pletina icon="iconFw" />
+  <Pletina icon="iconStop" />
+</template>
+
+<style scoped></style>
---
+++ src/components/Pletina.vue
@@ -0,0 +1,68 @@
+<script setup>
+    import iconPlay from '@/assets/icon-play.png'
+    import iconStop from '@/assets/icon-stop.png'
+    import iconPause from '@/assets/icon-pause.png'
+    import iconFw from '@/assets/icon-fw.png'
+    import iconBw from '@/assets/icon-bw.png'
+
+    const icons = {
+        iconPlay,
+        iconStop,
+        iconPause,
+        iconFw,
+        iconBw
+    }
+
+    const props = defineProps({
+        icon: {
+            type: String,
+            required: true
+        }
+    })
+
+    const emit = defineEmits(['clickIcon'])
+
+    function iconFunction(icon){
+        switch (icon) {
+            case 'iconPlay':
+                play()
+                break
+
+            case 'iconStop':
+                stop()
+                break
+        
+            default:
+                break;
+        }
+    }
+
+    function play(){
+        console.log('Let\'s play')
+        emit('clickIcon')
+    }
+
+    function stop(){
+        console.log('Please Don\'t stop the music');
+    }
+
+</script>
+
+<template>
+    <img
+        :src="icons[icon]"
+        @click="iconFunction(icon)"
+        class="icon-player"
+        alt="icon"
+    >
+</template>
+
+<style scoped>
+    .icon-player{
+        width: 32px;
+        cursor: pointer;
+    }
+        .icon-player:hover {
+            filter: invert(90%);
+        }
+</style>
